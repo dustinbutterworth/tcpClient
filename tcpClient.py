b3cf4751f99dd8dcd4d30559b40245d267598f60
Dustin Butterworth <51181391+dustinbutterworth@users.noreply.github.com>
2020-03-12 12:40:59 -0400

    Create tcpClient.py

---
+++ tcpClient.py
@@ -0,0 +1,20 @@
+import socket
+# From Black Hat Python: Python Programming for Hackers and Pentesters 1st Edition by Justin Seitz
+
+target_host = "www.google.com"
+target_port = 80
+data = "GET / HTTP/1.1\r\nHost:google.com\r\n\r\n"
+
+# create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect to the client
+client.connect((target_host,target_port))
+
+# send some data
+client.send(data.encode())
+
+# receive some data
+response = client.recv(4096)
+
+print(response)
